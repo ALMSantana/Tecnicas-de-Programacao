e0ddafd7fd54395a8548ed73ef42b9ed0d0106f9
andrelms91 <andrelms@univali.br>
2021-09-21 10:34:33 -0300

    StartRepo

--- 950/Exercicio1.py
+++
@@ -1,9 +0,0 @@
-def ehImpar(numero : int):
-    return numero % 2 != 0
-
-print("O valor é impar: ", ehImpar("batata"))
-print("O valor é impar: ", ehImpar(4))
-valor = 19
-print("O valor é impar: ", ehImpar(valor))
-print("O valor é impar: ", ehImpar(6))
-print("O valor é impar: ", ehImpar(9))
