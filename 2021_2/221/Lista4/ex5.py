97da6202cb111184269ba8af65320c0c85850d37
andrelms91 <andrelms@univali.br>
2021-09-27 21:52:10 -0300

    Colecttion Content and ex8/ex5 (221) :sparkles:

--- 2021_2/221/Lista4/ex5.py
+++ 2021_2/221/Lista4/ex5.py
@@ -1,14 +1,17 @@
 #Crie uma função que receba o nome e o sobrenome de uma pessoa e retorne os dois nomes 
 # concatenados (nome completo)
 #Ex.: envio André e envio Santana e a função retorna André Santana
-
 def concatenarNome(nome : str, sobrenome : str):
     return nome + " " + sobrenome 
 
+def main():
+    name = input("Digite o seu nome: ")
+    surName = input("Digite o seu sobrenome: ")
 
-name = input("Digite o seu nome: ")
-surName = input("Digite o seu sobrenome: ")
+    nomeCompleto = concatenarNome(name, surName)
+    print("O nome completo é: " + nomeCompleto)
+    print("O nome completo é: {}".format(nomeCompleto))
 
-nomeCompleto = concatenarNome(name, surName)
-print("O nome completo é: " + nomeCompleto)
-print("O nome completo é: {}".format(nomeCompleto))
+if __name__ == "__main__":
+    main()
+   
---
+++ 2021_2/Conteudos/Colecoes/lista.py
@@ -0,0 +1,6 @@
+listaAnos = [2018, 2019, 2020, 2021]
+listaMedias = [9.5, 9.8, 10.0, 9.3]
+
+listaAnos.append(2017)
+
+print(listaAnos)
